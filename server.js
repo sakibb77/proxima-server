cb88c671f6069272e8e3d1a6480eb223d9f1fb70
sakib biswas <sakibbiswas418@gmail.com>
2023-02-20 05:31:29 -0800

    cors added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const projectRoutes = require("./routes/projectRoute");
 
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
